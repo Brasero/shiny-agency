6a44ef3d4046d644b824f9fa16ed265f0b07c5da
Brasero <brandonricci4@gmail.com>
2021-09-03 18:38:02 +0200

    adding React-router

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg' 
+
 import '../../App.css'
 
 function Home() {
@@ -6,7 +6,6 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
